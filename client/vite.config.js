d147256397a61cf0b9c6eedf7b4cc8c3689958f9
Gehad Salem Abd El-shafy <35219807+GehadSalem@users.noreply.github.com>
2024-09-06 17:25:31 +0300

    edit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'http://localhost:5000',
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: 'build',
   },
   plugins: [react()],
 })
